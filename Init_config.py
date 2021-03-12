58e645fce12a7d711e46fd86f80b80abf436ebfc
Druzai <j7world@yandex.ru>
2021-03-12 14:43:57 +0300

    Refactored Init_config.py: Made some methods and variables private

--- Init_config.py
+++ Init_config.py
@@ -3,27 +3,27 @@ from json import dump, load
 
 
 class Config:
-    config_name = "config.json"
-    config_dict = {}
+    __config_name = "config.json"
+    __config_dict = {}
 
     def __init__(self):
-        if isfile(self.config_name):
-            with open(file=self.config_name, mode="r", encoding="utf-8") as config_file:
-                self.config_dict = load(config_file)
+        if isfile(self.__config_name):
+            with open(file=self.__config_name, mode="r", encoding="utf-8") as config_file:
+                self.__config_dict = load(config_file)
             print("Файл настроек загружен!")
         else:
-            self.set_up_config()
+            self.__set_up_config()
 
-    def set_up_config(self):
-        print(f"Файл '{self.config_name}' не найден! Настраиваем новый!")
-        self.set_token()
+    def __set_up_config(self):
+        print(f"Файл '{self.__config_name}' не найден! Настраиваем новый!")
+        self.__set_token()
 
-        with open(file=self.config_name, mode="w", encoding="utf-8") as config_file:
-            dump(self.config_dict, config_file, indent=2)
+        with open(file=self.__config_name, mode="w", encoding="utf-8") as config_file:
+            dump(self.__config_dict, config_file, indent=2)
         print("Файл настроек сохранён!")
 
-    def set_token(self):
-        self.config_dict["token"] = input("Введите токен vk бота: ")
+    def __set_token(self):
+        self.__config_dict["token"] = input("Введите токен vk бота: ")
 
     def get_token(self):
-        return self.config_dict["token"]
+        return self.__config_dict["token"]

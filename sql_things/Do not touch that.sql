9f60f217008b08864675ce1ef2ef0f02ce07a673
Valentin <samonchev001@gmail.com>
2021-03-18 18:20:41 +0300

    Updated some sql things (schema)

---
+++ sql_things/Do not touch that.sql
@@ -0,0 +1,21 @@
+
+USE test;
+
+CREATE TABLE users2(
+    Group1 int,
+    Data1 int,
+    week_2 int,
+    day_of_week_id int,
+);
+CREATE table day_of_week(
+		id int,
+		Subject1 string, //node??
+		Class_number String,
+		Teacher string,
+        Time1 time,
+        link String,
+        Node id
+    );
+//INSERT INTO users (firstname, age) VALUES ('Tom', 34);
+
+//SELECT * FROM users;

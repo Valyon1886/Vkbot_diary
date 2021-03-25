a49fa75380a6453bbff6dab11ffb9472f3a8ca1b
Druzai <j7world@yandex.ru>
2021-03-25 16:42:26 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Vkbot_diary',
+    version='1.0',
+    description='Useful bot for VK',
+    author='Valyon, Andrew, Sergey',
+    author_email='astyn37@gmail.com',
+    packages=['Vkbot_diary'],
+    install_requires=[s for s in
+                      [line.split('#', 1)[0].strip(' \t\n') for line in open('requirements.txt', 'r').readlines()] if
+                      s != '']
+)

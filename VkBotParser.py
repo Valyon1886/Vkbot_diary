b06e13ea15e9a5aec73b62e8431a9a2ccc3ea9c4
Druzai <g9code@yandex.ru>
2023-02-11 02:36:25 +0300

    Updated if to match usual schedule

--- VkBotParser.py
+++ VkBotParser.py
@@ -142,8 +142,8 @@ class Parser:
         tables_count = len(result_links)
 
         for x in result_links:
-            # all(s not in x for s in ["zach_", "_zachety"])
-            if any(s in x for s in ["zach_", "_zachety"]) and search(r"\d([-_])?(kurs|k)[^/]*\.xls", x.lower()):
+            # any(s in x for s in ["zach_", "_zachety"])
+            if all(s not in x for s in ["zach_", "_zachety", "_ekzameny"]) and search(r"\d([-_])?(kurs|k)[^/]*\.xls", x.lower()):
                 for _try in range(number_of_tries):
                     req = get(x)
                     if req.status_code == 200:
